33fd6b03b2560a6e72bb0f2e4b9bf0c8d5dc1dd1
damattag <guilherme.damatta@yahoo.com.br>
2024-07-19 18:21:59 -0300

    feat: add obstacles

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
-from settings import SCREEN_WIDTH, SCREEN_HEIGHT
+from settings import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_IMAGE
 from functions.move import move_player
 from characters.player import Player
+from characters.static_objects import StaticObject
 import pygame
 import sys
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('My game')
+pygame.display.set_caption('HellCInfe')
 
-character_img = pygame.image.load('assets/player.png')
+character_img = pygame.image.load(PLAYER_IMAGE)
 
 character = Player(character_img, 0, 300)
+rock = StaticObject(character_img, 250, 400)
 
 running = True
 while running:
@@ -23,9 +25,10 @@ while running:
 
     character.update()
 
-    screen.fill((0, 0, 0))
+    screen.fill((0, 45, 0))
 
     character.draw(screen)
+    rock.draw(screen)
 
     pygame.display.flip()
 
--- src/settings.py
+++ src/settings.py
@@ -1,2 +1,4 @@
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
+
+PLAYER_IMAGE = 'assets/characters/player.png'
